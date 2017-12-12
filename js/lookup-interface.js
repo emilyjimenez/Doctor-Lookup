0fd5abe2faa6145ec04dd70cedd76ef3a62a33e6
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-12-12 04:49:30 -0800

    create reload button.

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -2,7 +2,7 @@ import { Lookup } from './../js/lookup.js';
 
 let formatPhone = function(phone) {
   return phone.slice(0,3)+"-"+phone.slice(3,6)+"-"+phone.slice(6);
-}
+};
 
 let displayData = function(response) {
   $("#result").show();
@@ -14,7 +14,7 @@ let displayData = function(response) {
          for (let j = 0; j < index.practices.length; j++) {
           let practice = index.practices[j];
           let cityStateZip = (practice.visit_address.city + `, ` + practice.visit_address.state + ` ` + practice.visit_address.zip);
-          let googleResult = `https://www.google.com/search?q=${practice.name}${cityStateZip}`
+          let googleResult = `https://www.google.com/search?q=${practice.name}${cityStateZip}`;
           let phone = practice.phones[0].number;
           let phoneDisplay = formatPhone(phone);
           let website = practice.website;
@@ -24,8 +24,14 @@ let displayData = function(response) {
             } else {
               $("#result").append(`<p class="practice"><a href="${website}">${practice.name}</a></p>`);
             }
+
             $("#result").append(`<p class="address">${practice.visit_address.street}</p><p class="address"> ${cityStateZip}</p>`);
-            $("#result").append(`<p class="phone">` + phoneDisplay + `</p>`);
+            $("#result").append(`<span class="phone">` + phoneDisplay + `</span>`);
+            if (practice.accepts_new_patients = true) {
+              $("#result").append(`<span class="accepting">  Accepting patients!</span>`);
+            } else {
+              $("#result").append(`<span class="not-accepting">  NOT accepting new patients</span>`);
+            }
          }
       }
     } else {
@@ -35,13 +41,23 @@ let displayData = function(response) {
 
 let error = function(error) {
   $("#result").text("Something went wrong, no doctors found!");
-}
+};
 
 $(document).ready(function() {
   let newLookup = new Lookup();
   $("#lookup-name").submit(function(event) {
     event.preventDefault();
     let name = $("#name").val();
-    newLookup.getData(name, displayData);
+    newLookup.getData(name, displayData, error);
+  });
+
+  $("#lookup-issue").submit(function(event) {
+    event.preventDefault();
+    let issue = $("#issue").val();
+    newLookup.getData(issue, displayData, error);
+  });
+
+  $("#reload").click(function() {
+  location.reload();
   });
 });
